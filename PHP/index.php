12fbe1319aea4901deab9f48e0052818a7e21302
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-03-06 22:15:05 -0600

    nothing to say

---
+++ PHP/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+<link rel="stylesheet" type="text/css" href="../CSS/index.css">
+
+<!--  Navigation  -->
+<div class="topnav">
+    <nav class="w3-bar w3-black center">
+        <a class="center"href="#" class="w3-button w3-bar-item">Home</a>
+        <a class="center"href="#band" class="w3-button w3-bar-item">Band</a>
+        <a class="center"href="#tour" class="w3-button w3-bar-item">Tour</a>
+        <a class="center"href="#contact" class="w3-button w3-bar-item">Contact</a>
+    </nav>
+</div>
+
+<head>
+
+    
+    
+</head>
+
+
+</html>
---
+++ PHP/test.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+
+
+
+ <body>
+    <?php
+    // Comment 
+    # Comment
+    /*
+    Comment
+     */
+
+    // Create a variable
+    # '$'variableName = value;
+
+    $x = 5;
+
+    $txt = "PHP";
+    echo "I love $txt!";
+    ?>
+
+    
+    <?php 
+
+
+
+    // Declaring Functions
+    function firstFunction() {
+        global $x;
+        $x = $x + $x;
+        echo "<p> The variable x in my first function is equal to $x</p>";
+    }
+    
+    firstFunction();
+
+    // All global variables are stored in an array called
+    $GLOBALS['x'];
+    # The index is the name of the variable
+
+    function staticTest(){
+        static $y = 1;
+        echo "$y</br>";
+        $y++;
+    }
+
+    staticTest();
+    staticTest();
+    staticTest();
+    
+    ?>
+
+    <!--   Variables     -->
+    <?php 
+    
+    # Float
+    $fl = 10.569;
+    var_dump($fl);
+    echo "<h1>$fl</br></br></h1>";
+
+    # Bool
+    $bl = true;
+    var_dump($bl);
+    echo "</br>$bl</br></br>";
+
+    # Array 
+    $cars = array("Volvo", "BMW", "Toyota");
+    var_dump($cars);
+    echo "</br>$cars[0]</br>";
+    
+    # Objects
+    class Drink {
+        public $type;
+        public $APC;
+        public function __construct($type, $APC){
+            $this->type = $type;
+            $this->APC = $APC;
+        }
+        public function message(){
+            return "</br>This drink is a " . $this->type . " and contatins " . $this->APC . "% alcohol";
+        }
+    }
+
+    $beer = new Drink("Beer", 4.5);
+    echo $beer -> message();
+    echo "</br>";
+    $ginTonic = new Drink("Gin and Tonic", 13.5);
+    echo $ginTonic -> message();
+
+
+    # Null
+    $nul = null;
+    echo "</br>";
+    var_dump($nul);
+    ?>
+
+    <!--  Numbers  -->
+    <?php 
+    
+    echo "<br>";
+
+    # Check if value is numeric
+    $x = 589;
+    var_dump(is_numeric($x));
+    echo "<br>";
+    $x = "hello5";
+    var_dump(is_numeric($x));
+
+    $x = "50";
+
+    ?>
+
+    <!-- Constants -->
+    <?php 
+    define("HEADER", "<h1>My name is Keaton Love and you should hire me.</h1>");
+    echo HEADER;
+    ?>
+
+<body>
+
+</html>
